abab8bea77e7d283022127aa96a4f4fef1d2bdf7
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-12 10:28:47 -0400

    Changed world to Earth

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.print.outln ("Hello");
-        System.print.outln ("World");
+        System.print.outln ("Earth");
 
     }
 }
