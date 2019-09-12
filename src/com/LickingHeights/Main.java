206e2ee865c6de92ab5c531788142d3e7c6400c9
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-12 10:30:13 -0400

    Changed hello to Greetings

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,7 +3,7 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-        System.print.outln ("Hello");
+        System.print.outln ("Greetings");
         System.print.outln ("Earth");
 
     }
