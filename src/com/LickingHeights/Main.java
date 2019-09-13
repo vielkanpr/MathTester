e3a116277c9a78e2242e5d3c5c23c1c659649f0c
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-13 10:00:05 -0400

    Initialized firstNumber as 110

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -3,17 +3,19 @@ package com.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Greetings");
-        System.out.println("Earth");
+        //System.out.println("Greetings");
+        //System.out.println("Earth");
         String firstWord;
         String secondWord;
-        int firstNumber , secondNumber;
+        int firstNumber,secondNumber;
 
 
         firstWord = "Rainbow";
         secondWord = "Gold";
-        firstNumber = " ";
-        secondNumber = " ";
+        firstNumber = 110;
+        secondNumber = ;
 
+        System.out.println(firstWord+secondWord);
+        System.out.println(firstNumber+secondNumber);
     }
 }
