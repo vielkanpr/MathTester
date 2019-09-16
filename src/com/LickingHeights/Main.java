84a27818a3f57b26b1526a68b5861fc33a146b8d
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-16 09:44:40 -0400

    block commented sequences

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,19 +7,28 @@ public class Main {
         //System.out.println("Earth");
         String firstWord;
         String secondWord;
-        int firstNumber,secondNumber;
+        int firstNumber,secondNumber, total;
 
 
         firstWord = "Rainbow";
         secondWord = "Gold";
         firstNumber = 10;
         secondNumber = 200;
+        total = firstNumber+ secondNumber;
 
-        System.out.println(firstWord+secondWord);
-        System.out.println(firstNumber+secondNumber);
-        System.out.println(firstWord+firstNumber);
-        System.out.println(secondNumber-firstNumber);
-        System.out.println(firstNumber-secondNumber);
+
+/*
+        System.out.println("firstWord + secondWord is   "+ (firstWord+secondNumber));
+        System.out.println("firstNumber + secondNumber is   "+ ( firstNumber+ secondNumber));
+        System.out.println("firstWord + firstNumber is   " + (firstWord+ firstNumber));
+        System.out.println("secondNumber - firstNumber is   " +(secondNumber-firstNumber));
+        System.out.println("firstNumber - secondNumber is     "+ (firstNumber-secondNumber));
+        System.out.println("secondNumber / firstNumber is   " + (secondNumber/ firstNumber));
+        System.out.println(" the modulus of the secondNumber by the firstNumber"+
+                " is    " +(secondNumber%firstNumber ));*/
+        total = firstNumber+ secondNumber;
+        System.out.println(total);
+        total = total + 12;
 
 
     }
